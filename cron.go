4a2e30cc6413267445236012b0fe837c5d4730d0
Burak Köken <burakkokenn@gmail.com>
2021-05-20 23:55:18 +0200

    Added NextTime method for calculating the next time for given cron expression.

--- cron.go
+++ cron.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"math/bits"
 	"strconv"
 	"strings"
+	"time"
 )
 
 var (
@@ -39,7 +41,7 @@ var (
 	dayOfWeek  = fieldType{cronFieldDayOfWeek, 0, 6}
 )
 
-var cronFields = []fieldType{
+var cronFieldTypes = []fieldType{
 	second,
 	minute,
 	hour,
@@ -61,15 +63,18 @@ func newValueRange(min int, max int) valueRange {
 }
 
 type cronFieldBits struct {
-	typ  fieldType
-	bits uint64
+	Typ  fieldType
+	Bits uint64
 }
 
 func newFieldBits(typ fieldType) *cronFieldBits {
 	return &cronFieldBits{
-		typ: typ,
+		Typ: typ,
 	}
 }
+
+const maxAttempts = 366
+const mask = 0xFFFFFFFFFFFFFFFF
 
 type CronExpression struct {
 	fields []*cronFieldBits
@@ -79,6 +84,57 @@ func newCronExpression() *CronExpression {
 	return &CronExpression{
 		make([]*cronFieldBits, 0),
 	}
+}
+
+func (expression *CronExpression) NextTime(t time.Time) time.Time {
+
+	t = t.Add(1*time.Second - time.Duration(t.Nanosecond())*time.Nanosecond)
+
+	for i := 0; i < maxAttempts; i++ {
+		result := expression.next(t)
+
+		if result.IsZero() || result.Equal(t) {
+			return result
+		}
+
+		t = result
+	}
+
+	return time.Time{}
+}
+
+func (expression *CronExpression) next(t time.Time) time.Time {
+	for _, field := range expression.fields {
+
+		temp := t
+		current := getTimeValue(temp, field.Typ.Field)
+
+		next := setNextBit(field.Bits, current)
+
+		if next == -1 {
+			amount := field.Typ.MaxValue - current + 1
+			temp = addTime(temp, field.Typ.Field, amount)
+			next = setNextBit(field.Bits, 0)
+		}
+
+		if next == current {
+			return t
+		} else {
+			count := 0
+			current := getTimeValue(temp, field.Typ.Field)
+			for ; current != next && count < maxAttempts; count++ {
+
+			}
+
+			if count >= maxAttempts {
+				return time.Time{}
+			}
+
+		}
+
+	}
+
+	return time.Time{}
 }
 
 func ParseCronExpression(expression string) (*CronExpression, error) {
@@ -94,8 +150,8 @@ func ParseCronExpression(expression string) (*CronExpression, error) {
 
 	cronExpression := newCronExpression()
 
-	for index, cronField := range cronFields {
-		value, err := parseField(fields[index], cronField)
+	for index, cronFieldType := range cronFieldTypes {
+		value, err := parseField(fields[index], cronFieldType)
 
 		if err != nil {
 			return nil, err
@@ -131,7 +187,12 @@ func parseField(value string, fieldType fieldType) (*cronFieldBits, error) {
 		if slashPos != -1 {
 			rangeStr := field[0:slashPos]
 
-			valueRange = parseRange(rangeStr, fieldType)
+			var err error
+			valueRange, err = parseRange(rangeStr, fieldType)
+
+			if err != nil {
+				return nil, err
+			}
 
 			if strings.Index(rangeStr, "-") == -1 {
 				valueRange = newValueRange(valueRange.MinValue, fieldType.MaxValue)
@@ -139,7 +200,6 @@ func parseField(value string, fieldType fieldType) (*cronFieldBits, error) {
 
 			stepStr := field[slashPos+1:]
 
-			var err error
 			step, err = strconv.Atoi(stepStr)
 
 			if err != nil {
@@ -151,48 +211,66 @@ func parseField(value string, fieldType fieldType) (*cronFieldBits, error) {
 			}
 
 		} else {
-			valueRange = parseRange(field, fieldType)
+			var err error
+			valueRange, err = parseRange(field, fieldType)
+
+			if err != nil {
+				return nil, err
+			}
 		}
 
 		if step > 1 {
 			for index := valueRange.MinValue; index <= valueRange.MaxValue; index += step {
-				cronFieldBits.bits |= 1 << index
+				cronFieldBits.Bits |= 1 << index
 			}
 			continue
 		}
 
 		if valueRange.MinValue == valueRange.MaxValue {
-			cronFieldBits.bits |= 1 << valueRange.MinValue
+			cronFieldBits.Bits |= 1 << valueRange.MinValue
 		} else {
-			cronFieldBits.bits = ^(math.MaxUint64 << (valueRange.MaxValue + 1)) & (math.MaxUint64 << valueRange.MinValue)
+			cronFieldBits.Bits = ^(math.MaxUint64 << (valueRange.MaxValue + 1)) & (math.MaxUint64 << valueRange.MinValue)
 		}
 	}
 
 	return cronFieldBits, nil
 }
 
-func parseRange(value string, fieldType fieldType) valueRange {
+func parseRange(value string, fieldType fieldType) (valueRange, error) {
 	if value == "*" {
-		return newValueRange(fieldType.MinValue, fieldType.MaxValue)
+		return newValueRange(fieldType.MinValue, fieldType.MaxValue), nil
 	} else {
 		hyphenPos := strings.Index(value, "-")
 
 		if hyphenPos == -1 {
-			result := checkValidValue(value, fieldType)
+			result, err := checkValidValue(value, fieldType)
 
-			return newValueRange(result, result)
+			if err != nil {
+				return valueRange{}, err
+			}
+
+			return newValueRange(result, result), nil
 		} else {
 			maxStr := value[hyphenPos+1:]
 			minStr := value[0:hyphenPos]
 
-			min := checkValidValue(minStr, fieldType)
-			max := checkValidValue(maxStr, fieldType)
+			min, err := checkValidValue(minStr, fieldType)
+
+			if err != nil {
+				return valueRange{}, err
+			}
+			var max int
+			max, err = checkValidValue(maxStr, fieldType)
+
+			if err != nil {
+				return valueRange{}, err
+			}
 
 			if fieldType.Field == cronFieldDayOfWeek && min == 7 {
 				min = 0
 			}
 
-			return newValueRange(min, max)
+			return newValueRange(min, max), nil
 		}
 	}
 }
@@ -208,20 +286,69 @@ func replaceOrdinals(value string, list []string) string {
 	return value
 }
 
-func checkValidValue(value string, fieldType fieldType) int {
+func checkValidValue(value string, fieldType fieldType) (int, error) {
 	result, err := strconv.Atoi(value)
 
 	if err != nil {
-		panic(err)
+		return 0, fmt.Errorf("the value in field %s must be number : %s", fieldType.Field, value)
 	}
 
 	if fieldType.Field == cronFieldDayOfWeek && result == 0 {
-		return result
+		return result, nil
 	}
 
 	if result >= fieldType.MinValue && result <= fieldType.MaxValue {
-		return result
+		return result, nil
 	}
 
-	panic("value is not valid")
+	return 0, fmt.Errorf("the value in field %s must be between %d and %d", fieldType.Field, fieldType.MinValue, fieldType.MaxValue)
+}
+
+func getTimeValue(t time.Time, field cronField) int {
+
+	switch field {
+	case cronFieldSecond:
+		return t.Second()
+	case cronFieldMinute:
+		return t.Minute()
+	case cronFieldHour:
+		return t.Hour()
+	case cronFieldDayOfMonth:
+		return t.Day()
+	case cronFieldMonth:
+		return int(t.Month())
+	case cronFieldDayOfWeek:
+		return int(t.Weekday())
+	}
+
+	panic("unreachable code")
+}
+
+func addTime(t time.Time, field cronField, value int) time.Time {
+	switch field {
+	case cronFieldSecond:
+		return t.Add(time.Duration(value) * time.Second)
+	case cronFieldMinute:
+		return t.Add(time.Duration(value) * time.Minute)
+	case cronFieldHour:
+		return t.Add(time.Duration(value) * time.Hour)
+	case cronFieldDayOfMonth:
+		return t.AddDate(0, 0, value)
+	case cronFieldMonth:
+		return t.AddDate(0, value, 0)
+	case cronFieldDayOfWeek:
+		return t.AddDate(0, 0, value)
+	}
+
+	panic("unreachable code")
+}
+
+func setNextBit(bitsValue uint64, index int) int {
+	result := bitsValue & (mask << index)
+
+	if result != 0 {
+		return bits.TrailingZeros64(result)
+	}
+
+	return -1
 }
