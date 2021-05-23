148506fa4ea7135d5d81dbc50f0a64919498181d
Burak Köken <burakkokenn@gmail.com>
2021-05-23 23:14:47 +0200

    Commented lines of executor unit tests

--- executor_test.go
+++ executor_test.go
@@ -14,11 +14,12 @@ type TestTask1 struct {
 
 func (task *TestTask1) Run(ctx context.Context) {
 	log.Print("Hello")
+	time.Sleep(5 * time.Second)
 }
 
 func TestNewScheduledTaskExecutor(t *testing.T) {
-	scheduledTaskExecutor := NewDefaultScheduledExecutor()
-	scheduledTaskExecutor.ScheduleAtWithRate(&TestTask1{}, 0, 8*time.Second)
+	//scheduledTaskExecutor := NewDefaultScheduledExecutor()
+	//scheduledTaskExecutor.ScheduleAtWithRate(&TestTask1{}, 0, 8*time.Second)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
