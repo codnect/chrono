f1f27f508742f9dbd682c8976932e1dedc645fa1
Burak Köken <burakkokenn@gmail.com>
2021-05-23 19:22:08 +0200

    Added executor unit test.

--- executor_test.go
+++ executor_test.go
@@ -9,11 +9,16 @@ import (
 	"time"
 )
 
+type TestTask1 struct {
+}
+
+func (task *TestTask1) Run(ctx context.Context) {
+	log.Print("Hello")
+}
+
 func TestNewScheduledTaskExecutor(t *testing.T) {
-	scheduledTaskExecutor := NewScheduledTaskExecutor()
-	scheduledTaskExecutor.ScheduleWithFixedDelay(func(ctx context.Context) {
-		log.Print("hi!")
-	}, 0, 5*time.Second)
+	scheduledTaskExecutor := NewDefaultScheduledExecutor()
+	scheduledTaskExecutor.ScheduleAtWithRate(&TestTask1{}, 0, 8*time.Second)
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
